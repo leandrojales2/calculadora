f90edae4ff3a37cdd916d1a14f545884c005ec8a
leandrojales2 <desenvolvedorjales@yahoo.com>
2024-10-28 19:30:59 -0300

    Criação de uma calculadora para desafio de código

---
+++ src/components/Input/styles.js
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+
+export const InputContainer = styled.div`
+    width: 100%;
+    height: 3rem;
+    background-color: #000000;
+    border: 1px solid #CDCDCD;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    font-size: 24px;
+    font-family: 'Roboto';
+
+    input {
+        width: 90%;
+        height: 2.4rem;
+        background-color: #;
+        border: 0;
+        padding-right: 12px;
+        text-align: right;
+
+        font-size: 24px;
+        font-family: 'Roboto';
+        color: #FFFFFF; 
+    }
+`
